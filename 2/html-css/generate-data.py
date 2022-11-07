a71a26a119883ff2a6a097648219bb4b45ce9dff
Henrik 'Catears' Adolfsson <catears13@gmail.com>
2022-11-07 20:51:53 +0100

    Tooootally solve the problem with html + CSS

--- 2/html-css/generate-data.py
+++ 2/html-css/generate-data.py
@@ -2,23 +2,34 @@ import sys
 import os
 sys.setrecursionlimit(2500)
 
-filename = 'test-input'
+filename = 'input'
 
 with open(filename, 'r') as f:
     content = [x.strip() for x in f.readlines()]
 
 commands = [(x.split(' ')[0], int(x.split(' ')[1])) for x in content]
 
-the_divs = []
+the_divs = {
+    'forward': [],
+    'up': [],
+    'down': []
+}
 for command, distance in commands:
-    the_divs.append(f'<div data-command="{command}" data-distance="{distance}"></div>')
-
-div_string = '\n'.join(the_divs)
+    the_divs[command].append(f'<div data-command="{command}" data-distance="{distance}"></div>')
 
 with open('index-template.html', 'r') as f:
     content = f.read()
 
-updated_content = content.replace('[[DATA]]', div_string)    
+keys = the_divs.keys()
+for key in keys:
+    inner_divs = '\n'.join(the_divs[key])
+    full_div = f'<div class="command-{key}">{inner_divs}</div>'
+    the_divs[key] = full_div
+
+dive_div = the_divs['forward'] + '\n' + the_divs['down']
+surface_div = the_divs['forward'] + '\n' + the_divs['up']
+updated_content = content.replace('[[DIVE-DATA]]', dive_div)
+updated_content = updated_content.replace('[[SURFACE-DATA]]', surface_div)
 
 with open('index.html', 'w') as f:
     f.write(updated_content)
@@ -27,4 +38,4 @@ generated_css = []
 
 with open('helper-vars.css', 'w') as f:
     for idx in range(0, 100):
-        print('div[data-distance="{idx}"] {{ --distance: {idx}; --distance-as-string: "{idx}" }}'.format(idx=idx), file=f)
+        print('div[data-distance="{idx}"] {{ --distance: {idx}px; --distance-as-string: "{idx}" }}'.format(idx=idx), file=f)
