63beea6459f7f3f5f3c40ab4818142e3aa4b600e
Henrik 'Catears' Adolfsson <catears13@gmail.com>
2022-11-07 18:22:13 +0100

    Save

--- nth-child-loop-generator.py
+++ nth-child-loop-generator.py
@@ -1,10 +1,11 @@
-loops = 1000
+loops = 300
 
 with open('template/nth-child-loop.css', 'w') as f:
     for i in range(loops):
         print(':nth-child({0}) {{ --nth-child: {0} }}'.format(i), file=f)    
     print('', file=f)
     for i in range(loops):
-        print('[data-integer-value = "{0}"] {{ --integer-value: {0} }}'.format(i), file=f)    
-
-
+        print('[data-integer-value = "{0}"] {{ --integer-value: {0}; --str-value: "{0}" }}'.format(i), file=f)    
+    print('', file=f)
+    for i in range(loops):
+        print('div:has(> div[data-integer-value = "{0}"]) {{ --child-integer-value: {0}; --child-str-value: "{0}" }}'.format(i), file=f)
